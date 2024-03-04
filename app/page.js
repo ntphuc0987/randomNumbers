f3d4a4b5ddd77551efcca332b56b09ecd16fff75
ntphuc0987 <truelove309@gmail.com>
2024-03-04 10:05:59 +0700

    Add Current Time and edit CSS

--- app/page.js
+++ app/page.js
@@ -1,10 +1,22 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const now = new Date();
+  const [currentTime, setCurrentTime] = useState('');
   const [numberGroups, setNumberGroups] = useState([]);
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = new Date();
+      const formattedTime = `${now.getHours()}:${now.getMinutes()}`;
+      setCurrentTime(formattedTime);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+  
   const generateRandomNumbers = () => {
     const numbers = Array.from({ length: 99 }, (_, i) => i + 1);
     const shuffledNumbers = shuffleArray(numbers);
@@ -25,8 +37,10 @@ export default function Home() {
     }
     return array;
   };
+
+
   return (
-    <main className="flex flex-col h-screen items-center justify-center p-5">
+    <main className="flex flex-col items-center justify-center p-5">
       <div className="flex justify-center flex-col">
         <h1>Random Number Groups</h1>
         <button
@@ -36,10 +50,12 @@ export default function Home() {
           Generate Groups
         </button>
       </div>
-      <ul className="flex flex-wrap w-full justify-start border-">
+      <ul className="flex flex-wrap w-full justify-start p-0 m-0">
         {numberGroups.map((group, index) => (
-          <li key={index} className="p-5 w-3/12 text-center ">
+          <li key={index} className="p-5 w-full sm:w-6/12 md:w-3/12 text-center ">
             Group {index + 1}: {group.join(", ")}
+            <br />
+            <span className="bg-zinc-400 text-white rounded p-1 text-xs">{currentTime}</span>
           </li>
         ))}
       </ul>
